34cd021b200fd35e9efc21bfe0689ca9f9f4d1dd
fixorium <fixercoin.token@gmail.com>
2025-07-20 20:09:20 +0500

    Update wallet.js

--- src/wallet.js
+++ src/wallet.js
@@ -1,1 +1,7 @@
+export async function getWalletAddress() {
+  await window.keplr.enable("fixorium-devnet");
+  const signer = window.getOfflineSigner("fixorium-devnet");
+  const accounts = await signer.getAccounts();
+  return accounts[0].address;
+}
 
